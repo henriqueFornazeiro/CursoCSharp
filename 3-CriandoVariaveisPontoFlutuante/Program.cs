16c738beae1eb510e608f259ce246093bb19f826
HenriqueFornazeiro <HenriqueFornazeiro@LAPTOP-LHPD57HH>
2021-03-10 22:27:05 -0300

    Aula03 - Criando variáveis númericas. Trabalhando com criação e conversão de tipos.

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 - Criando variaveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+
+            Console.WriteLine(salario);
+
+            double idade;
+            idade = 15/2;
+
+            Console.WriteLine(idade);
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3");
+
+            double salario;
+            salario = 1200.50;
+
+            int salarioEmInt;
+            salarioEmInt = (int)salario;
+            Console.WriteLine("Salário convertido: "+salarioEmInt);
+
+            long idade; //long - 64bits;
+            idade = 13000000000;
+            Console.WriteLine(idade);
+
+            short qtdeProdutos; //short - 16bits;
+            qtdeProdutos = 150;
+            Console.WriteLine(qtdeProdutos);
+
+            float altura;
+            altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+        }
+    }
+}

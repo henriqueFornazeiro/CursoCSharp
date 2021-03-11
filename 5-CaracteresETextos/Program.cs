114b8fef7efbcb5eb543f28055fde6c867f3d9b3
HenriqueFornazeiro <HenriqueFornazeiro@LAPTOP-LHPD57HH>
2021-03-10 23:07:25 -0300

    Aula04-Trabalhando com variáveis de texto.

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caracteres e Textos");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine("somado e convertido: " + primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia";
+            Console.WriteLine(titulo);
+
+            string cursosProgramacao = 
+@"- .Net
+- Java
+- Javascript";
+            Console.WriteLine(cursosProgramacao);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 6");
+
+            int idade = 32;
+            int idadeX = idade;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeX);
+
+            Console.ReadLine();
+        }
+    }
+}

3066b134108d69959c31c28964d24ba145f155fa
HenriqueFornazeiro <HenriqueFornazeiro@LAPTOP-LHPD57HH>
2021-03-15 21:28:12 -0300

    Aula05-Controle de fluxo com IF

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Executando projeto 7");
+
+            int idadeJoaozinho = 16;
+            int qtdePessoas = 2;
+            if(idadeJoaozinho >= 18)
+            {
+                Console.WriteLine("Joãozinho possui mais de 18 anos");
+            }
+            else
+            {
+                if(qtdePessoas >= 2)
+                {
+                    Console.WriteLine("Joãozinho não possui mais de 18 anos. Mas está acompanhado. Pode entrar");
+                }
+                else
+                {
+                    Console.Write("Joãozinho não possui mais de 18 anos");
+                }
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

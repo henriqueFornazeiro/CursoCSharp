4a60cafb847d59b029a3ccb331dd7e5de227fc67
HenriqueFornazeiro <HenriqueFornazeiro@LAPTOP-LHPD57HH>
2021-03-15 21:28:12 -0300

    Aula05-Controle de fluxo com IF

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 8");
+
+            int idadeJoaozinho = 16;
+            int qtdePessoas = 2;
+            bool acompanhado = qtdePessoas >= 2;
+
+            if (idadeJoaozinho >= 18 || acompanhado)
+            {
+                Console.WriteLine("Joãozinho pode entrar");
+            }
+            else
+            {
+                Console.Write("Joãozinho não pode entrar");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9");
+
+            int idadeJoaozinho = 16;
+            int qtdePessoas = 2;
+            bool acompanhado = qtdePessoas >= 2;
+            string msgAdicional;
+
+            if (acompanhado)
+            {
+                msgAdicional = "Joãozinho está acompanhado";
+            }
+            else
+            {
+                msgAdicional = "Joãozinho não está acompanhado";
+            }
+            
+            if (idadeJoaozinho >= 18 && acompanhado)
+            {
+                Console.WriteLine("Joãozinho pode entrar");
+                Console.WriteLine(msgAdicional);
+            }
+            else
+            {
+                Console.Write("Joãozinho não pode entrar");
+                Console.WriteLine(msgAdicional);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
